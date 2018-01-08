063d88f86715233936728289e8253af1a3a76240
fmorroia <fabrizio.morroia@scai-consulting.grupposcai.it>
2018-01-08 12:58:53 +0100

    refactor: extract list elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
-import React from 'react';
+import React from "react";
 
-var App = React.createElement(
-  'div',
+var listDay1 = React.createElement(
+  "li",
   {},
+  React.createElement("h2", {}, "Giorno 1"),
   React.createElement(
-    'h1',
-    {
-      style: { background: '#efefef', padding: '10px' },
-      onClick: function() {
-        alert('miao');
-      }
-    },
-    'Corso React'
-  ),
-  React.createElement(
-    'ul',
+    "ul",
     null,
     React.createElement(
-      'li',
+      "li",
       null,
-      React.createElement('h2', null, 'Giorno 1'),
-      React.createElement(
-        'ul',
-        null,
-        React.createElement(
-          'li',
-          null,
-          React.createElement('a', { target: 'blank', href: 'https://reactjs.org/' }, 'React')
-        )
-      )
-    ),
-    React.createElement(
-      'li',
-      null,
-      React.createElement('h2', null, 'Giorno 2'),
-      React.createElement(
-        'ul',
-        null,
-        React.createElement(
-          'li',
-          null,
-          React.createElement('a', { target: 'blank', href: 'https://redux.js.org/' }, 'Redux')
-        )
-      )
+      React.createElement("a", { target: "blank", href: "https://reactjs.org/" }, "React")
     )
   )
+);
+
+var listDay2 = React.createElement(
+  "li",
+  {},
+  React.createElement("h2", {}, "Giorno 2"),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      React.createElement("a", { target: "blank", href: "https://redux.js.org/" }, "Redux")
+    )
+  )
+);
+
+var App = React.createElement(
+  "div",
+  {},
+  React.createElement(
+    "h1",
+    {
+      style: { background: "#efefef", padding: "10px" },
+      onClick: function() {
+        alert("miao");
+      }
+    },
+    "Corso React"
+  ),
+  React.createElement("ul", {}, listDay1, listDay2)
 );
 
 export default App;
