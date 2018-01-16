8866a08afc77278844dc7deffdb40e2f7cb0bd20
fmorroia <fabrizio.morroia@scai-consulting.grupposcai.it>
2018-01-16 12:19:48 +0100

    simple store implementation

--- src/reducers/artistReducers.js
+++ src/reducers/artistReducers.js
@@ -9,7 +9,30 @@ const artistSearch = (state = "", action) => {
   }
 };
 
-import { createStore } from "redux";
+//import { createStore } from "redux";
+
+const createStore = reducer => {
+  let state;
+  let listeners = [];
+
+  const getState = () => {
+    return state;
+  };
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(artistSearch);
 console.log("state: ", store.getState());
 
@@ -21,9 +44,5 @@ store.dispatch({ type: "SEARCH_APPEND", content: "a" });
 store.dispatch({ type: "SEARCH_APPEND", content: "b" });
 store.dispatch({ type: "SEARCH_APPEND", content: "c" });
 store.dispatch({ type: "SEARCH_REMOVE" });
-
-store.subscribe(() => {
-  console.log("state: ", store.getState());
-});
 
 export default artistSearch;
