a29c047843129f1d6254b386e81e81afd27b86bc
fmorroia <fabrizio.morroia@scai-consulting.grupposcai.it>
2018-01-16 11:59:57 +0100

    create the store, use getState and dispatch

--- src/reducers/artistReducers.js
+++ src/reducers/artistReducers.js
@@ -9,16 +9,21 @@ const artistSearch = (state = "", action) => {
   }
 };
 
-let state = artistSearch("", { type: "SEARCH_APPEND", content: "a" });
-console.log(state);
+import { createStore } from "redux";
+const store = createStore(artistSearch);
+console.log("state: ", store.getState());
 
-state = artistSearch(state, { type: "SEARCH_APPEND", content: "b" });
-console.log(state);
+store.dispatch({ type: "SEARCH_APPEND", content: "a" });
+console.log("state: ", store.getState());
+store.dispatch({ type: "SEARCH_APPEND", content: "b" });
+console.log("state: ", store.getState());
+store.dispatch({ type: "SEARCH_APPEND", content: "c" });
+console.log("state: ", store.getState());
+store.dispatch({ type: "SEARCH_REMOVE" });
+console.log("state: ", store.getState());
 
-state = artistSearch(state, { type: "SEARCH_APPEND", content: "c" });
-console.log(state);
-
-state = artistSearch(state, { type: "SEARCH_REMOVE" });
-console.log(state);
+store.subscribe(() => {
+  console.log("state: ", store.getState());
+});
 
 export default artistSearch;
